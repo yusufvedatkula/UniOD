1f88fb4aca9c1ec757a97106b7f43fee699153a9
Yusuf Kula <yusufkula@Yusufs-MacBook-Air.local>
2024-09-15 14:57:34 +0100

    add constant nav links

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'open_day_reminder', label: 'Open Day Reminder' },
+    { href: '/', key: 'favourites', label: 'Favourites' },
+    { href: '/', key: 'my_account', label: 'My Account' },
+];
