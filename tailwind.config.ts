742d382dd3feb15e8c96da173ef4e6769c129b41
Yusuf Kula <yusufkula@Yusufs-MacBook-Air.local>
2024-09-15 14:59:43 +0100

    new CSS, remove default code from layout and page.tsx, add icons for navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,14 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/preline/dist/*.js'
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  plugins: [
+    require('daisyui'),
+    require('preline/plugin')
+  ],
+  daisyui:{
+    themes: ["nord"]
   },
-  plugins: [],
 };
 export default config;
