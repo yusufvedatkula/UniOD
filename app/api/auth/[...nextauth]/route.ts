60fa2cf81ca783b95960e2647f4c40214f5280c3
Yusuf Kula <yusufkula@Yusufs-MacBook-Air.local>
2025-01-01 16:52:14 +0000

    Refactor NextAuth configuration by removing unnecessary export and consolidating options for cleaner code structure

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
+const handler = NextAuth({
   providers: [
     CredentialsProvider({
       name: "credentials",
@@ -46,10 +46,6 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/login",
   },
-};
-
-const handler = NextAuth({
-  ...authOptions,
 });
 
 export { handler as GET, handler as POST };
